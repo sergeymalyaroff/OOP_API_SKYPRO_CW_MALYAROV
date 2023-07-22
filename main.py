f06e9692a4e3481eb710c680e06ae1c2fc6e0579
Sergey Malyarov <sergey@malyaroff.com>
2023-07-22 13:21:00 +0300

    add superjob class

--- main.py
+++ main.py
@@ -4,67 +4,67 @@ import requests
 import json
 import os
 
-# Шаг 1: Определение абстрактного класса для работы с API сайтов с вакансиями
+
 class JobBoard(ABC):
+    """
+    Абстрактный класс для работы с API сайтов с вакансиями.
+    """
+
     @abstractmethod
     def get_vacancies(self, query):
         pass
 
-# Шаг 2: Определение классов для работы с конкретными платформами
+
 class HhruBoard(JobBoard):
     API_URL = "https://api.hh.ru/vacancies"
     HEADERS = {"User-Agent": "MyApp/1.0 (my-app-feedback@example.com)"}
 
     def get_vacancies(self, query):
         response = requests.get(self.API_URL, headers=self.HEADERS, params={"text": query})
+
         if response.status_code == 200:
             vacancies_data = json.loads(response.text)
             vacancies = []
+
             for data in vacancies_data.get('items', []):
-                title = data["name"]
-                url = data["alternate_url"]
-                salary = data["salary"]
-                description = data["snippet"]["responsibility"] if data.get("snippet") else ""
-                try:
-                    vacancy = Job(title, url, salary, description)
-                    vacancies.append(vacancy)
-                except ValueError as e:
-                    print(f"Ошибка создания вакансии: {e}")
+                title = data.get("name", "No title")
+                url = data.get("alternate_url", "No URL")
+                salary = data.get("salary", "No salary data")
+                description = data.get("snippet", {}).get("responsibility", "No description")
+                vacancies.append(Job(title, url, salary, description))
+
             return vacancies
         else:
             print(f"Ошибка получения данных: {response.status_code}")
-            return None
+            return []
 
 
 class SuperJobBoard(JobBoard):
     API_URL = "https://api.superjob.ru/2.0/vacancies/"
-    HEADERS = {
-        "X-Api-App-Id": "ваш_ключ_API"
-    }
+    HEADERS = {"X-Api-App-Id": "v3.r.137691035.923f9ca8f17abebb4aef7f41ffecf7f4a036c239.a5379584db956adef0765e7f3d0c3eccc51eed9a"}
 
     def get_vacancies(self, query):
         response = requests.get(self.API_URL, headers=self.HEADERS, params={"keywords": query})
+
         if response.status_code == 200:
             vacancies_data = json.loads(response.text)
+
             if "error" in vacancies_data:
                 print(f"Ошибка API: {vacancies_data['error']['message']}")
-                return None
+                return []
 
             vacancies = []
             for data in vacancies_data.get('objects', []):
-                title = data["profession"]
-                url = data["link"]
-                salary = data["payment"]
-                description = data["work"]
-                try:
-                    vacancy = Vacancy(title, url, salary, description)
-                    vacancies.append(vacancy)
-                except ValueError as e:
-                    print(f"Ошибка создания вакансии: {e}")
+                title = data.get("profession", "No title")
+                url = data.get("link", "No URL")
+                salary = data.get("payment", "No salary data")
+                description = data.get("work", "No description")
+                vacancies.append(Job(title, url, salary, description))
+
             return vacancies
         else:
             print(f"Ошибка получения данных: {response.status_code}")
-            return None
+            return []
 
 
 class Job:
@@ -74,8 +74,14 @@ class Job:
         self.salary = salary
         self.description = description
 
+    def __str__(self):
+        return f"Title: {self.title}\nURL: {self.url}\nSalary: {self.salary}\nDescription: {self.description}"
+
     def __lt__(self, other):
+        if not isinstance(other, Job):
+            raise ValueError("Both objects must be of type Job")
         return self.salary < other.salary
+
 
 class JobFile(ABC):
     @abstractmethod
@@ -89,6 +95,7 @@ class JobFile(ABC):
     @abstractmethod
     def delete_from_file(self, job_id):
         pass
+
 
 class JSONJobFile(JobFile):
     def __init__(self, file_name):
@@ -118,18 +125,31 @@ class JSONJobFile(JobFile):
             json.dump(data, file)
             file.truncate()
 
-def interact_with_user():
-    hh_api = HhruBoard()
-    hh_vacancies = hh_api.get_vacancies("Python")
 
-    json_saver = JSONJobFile("vacancies.json")
-    for vacancy in hh_vacancies:
-        json_saver.add_to_file(vacancy)
+def interact_with_user():
+    print("Выберите доску для поиска вакансий:")
+    print("1. hh.ru")
+    print("2. superjob.ru")
+    choice = input("> ")
+
+    if choice == '1':
+        api = HhruBoard()
+    elif choice == '2':
+        api = SuperJobBoard()
+    else:
+        print("Неверный выбор.")
+        return
+
+    query = input("Введите поисковый запрос: ")
+    vacancies = api.get_vacancies(query)
+
+    file_saver = JSONJobFile("vacancies.json")
+    for vacancy in vacancies:
+        file_saver.add_to_file(vacancy)
 
     print("Все вакансии сохранены. Вы можете их просмотреть, отфильтровать или удалить.")
 
-def main():
-    interact_with_user()
 
 if __name__ == "__main__":
-    main()
+    interact_with_user()
+
